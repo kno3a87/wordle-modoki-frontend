19e490b6304746b1b280c6b8820f47052446e87e
kuno <ayana.kuno@mixi.co.jp>
2022-02-05 18:30:56 +0900

    validation 追加

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wordle_modoki/feat/validator.dart';
 import 'package:wordle_modoki/theme/theme.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,6 +11,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final challengeCount = 5;
+  final wordCharLength = 7;
   final _focus = [true, false, false, false, false];
   final _textController = [
     TextEditingController(),
@@ -17,6 +19,13 @@ class _MyHomePageState extends State<MyHomePage> {
     TextEditingController(),
     TextEditingController(),
     TextEditingController(),
+  ];
+  final _formKey = [
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
   ];
 
   String inputWord = "";
@@ -31,34 +40,30 @@ class _MyHomePageState extends State<MyHomePage> {
       body: ListView.builder(
         itemCount: challengeCount,
         itemBuilder: (context, i) {
-          return _field(_textController, _focus, i);
+          return _field(i);
         },
       ),
     );
   }
 
-  Widget _field(
-    List<TextEditingController> controller,
-    List<bool> focus,
-    int index,
-  ) {
+  Widget _field(int index) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: SizedBox(
-              height: 50,
-              child: TextField(
+      child: Form(
+        key: _formKey[index],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: TextFormField(
                 autofocus: true,
                 minLines: 1,
                 maxLines: 1,
-                controller: controller[index],
+                controller: _textController[index],
                 decoration: InputDecoration(
                   filled: true,
                   // TOOD: 色作る
-                  fillColor: focus[index] ? Colors.white : Colors.grey,
+                  fillColor: _focus[index] ? Colors.white : Colors.grey,
                   border: OutlineInputBorder(
                     borderSide: const BorderSide(
                       width: 5,
@@ -68,48 +73,51 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 keyboardType: TextInputType.url,
-                enabled: focus[index],
+                enabled: _focus[index],
                 onChanged: (value) {
                   setState(() {
                     inputWord = value;
                   });
                 },
+                validator: (value) => validateWord(value, wordCharLength),
               ),
             ),
-          ),
-          // TODO: Mutation 呼ぶ
-          IconButton(
-            onPressed: !focus[index]
-                ? null
-                : () {
-                    setState(() {
-                      focus[index] = false;
-                      if (index < challengeCount - 1) {
-                        focus[index + 1] = true;
-                      }
-                    });
-                    showDialog(
-                      context: context,
-                      builder: (dialogContext) {
-                        return AlertDialog(
-                          title: const Text("ぷぷぷ"),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(dialogContext);
-                              },
-                              child: const Text("done"),
-                            )
-                          ],
-                        );
-                      },
-                    );
-                  },
-            icon: const Icon(
-              Icons.arrow_right_alt_outlined,
+            // TODO: Mutation 呼ぶ
+            IconButton(
+              onPressed: !_focus[index] ||
+                      _formKey[index].currentState == null ||
+                      !_formKey[index].currentState!.validate()
+                  ? null
+                  : () {
+                      setState(() {
+                        _focus[index] = false;
+                        if (index < challengeCount - 1) {
+                          _focus[index + 1] = true;
+                        }
+                      });
+                      showDialog(
+                        context: context,
+                        builder: (dialogContext) {
+                          return AlertDialog(
+                            title: const Text("ぷぷぷ"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(dialogContext);
+                                },
+                                child: const Text("done"),
+                              )
+                            ],
+                          );
+                        },
+                      );
+                    },
+              icon: const Icon(
+                Icons.star_rate,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
