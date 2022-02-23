aefb89578b06a4b9d095894abf9f202e07dc8135
kuno <ayana.kuno@mixi.co.jp>
2022-02-23 16:31:44 +0900

    色つけ

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,10 +4,12 @@ class WMColor {
   static const primaryColor = Color.fromRGBO(0x26, 0xc6, 0xda, 1.0);
   static const primaryLightColor = Color.fromRGBO(0x6f, 0xf09, 0xff, 1.0);
   static const primaryDarkColor = Color.fromRGBO(0x00, 0x95, 0xa8, 1.0);
-  static const secondaryColor = Color.fromRGBO(0x60, 0x7d, 0x8b, 1.0);
-  static const secondaryLightColor = Color.fromRGBO(0x8e, 0xac, 0xbb, 1.0);
-  static const secondaryDarkColor = Color.fromRGBO(0x34, 0x51, 0x5e, 1.0);
+  static const secondaryColor = Color.fromRGBO(0xfd, 0xd8, 0x35, 1.0);
+  static const secondaryLightColor = Color.fromRGBO(0xff, 0xff, 0x6b, 1.0);
+  static const secondaryDarkColor = Color.fromRGBO(0xc6, 0xa7, 0x00, 1.0);
   static const white = Color.fromRGBO(0xff, 0xff, 0xff, 1.0);
+  static const lightGray = Color.fromRGBO(0xcf, 0xcf, 0xcf, 1.0);
+  static const gray = Color.fromRGBO(0x9e, 0x9e, 0x9e, 1.0);
   static const black = Color.fromRGBO(0x00, 0x00, 0x00, 1.0);
 }
 
